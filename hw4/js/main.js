b969900229b957f73dd1a7011f4b0ef2ba11a5a1
KainoSilva <raxon1213@ukr.net>
2023-03-08 12:30:01 +0200

    Homework 4 Completed

---
+++ hw4/js/main.js
@@ -0,0 +1,20 @@
+let a = +prompt('Enter first number', '');
+let b = +prompt('Enter second number', '');
+let c = +prompt('Enter third number', '');
+
+let n = a + b + c;
+
+alert(n / 3);
+
+
+// let numFirst = +prompt('Enter first number', '');
+// let numSecond = +prompt('Enter second number', '');
+// let numThird = +prompt('Enter third number', '');
+
+// let nums = [numFirst, numSecond, numThird];
+
+// function getAverage(nums) {
+//   return nums.reduce((a, b) => a + b, 0) / nums.length;
+// }
+
+// alert(getAverage(nums));
